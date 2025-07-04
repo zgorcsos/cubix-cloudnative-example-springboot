839b13dfa29b8c9ea28460de6452844a59736862
Görcsös Zoltán <gorcsos.zoltan@gmail.com>
2025-07-04 19:39:43 +0200

    Replacing placeholders for Docker

--- docker-run-from-github.ps1
+++ docker-run-from-github.ps1
@@ -1,1 +1,1 @@
-docker run -p 8080:8080 --rm --name demo -it ghcr.io/USERNAME/cubix/cloudnative/demo:actions
+docker run -p 8080:8080 --rm --name demo -it ghcr.io/zgorcsos/cubix/cloudnative/demo:actions
